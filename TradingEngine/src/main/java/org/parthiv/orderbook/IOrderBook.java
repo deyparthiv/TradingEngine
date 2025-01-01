b19cd2cb94168ae72bea10383609918ab9ea48ed
Parthiv <94118901+deyparthiv@users.noreply.github.com>
2025-01-01 19:35:22 +0000

    documenting IOrderVisitor

--- TradingEngine/src/main/java/org/parthiv/orderbook/IOrderBook.java
+++ TradingEngine/src/main/java/org/parthiv/orderbook/IOrderBook.java
@@ -1,5 +1,7 @@
 package org.parthiv.orderbook;
 
+import java.security.Security;
+
 /**
  * Interface for the order book
  */
@@ -7,7 +9,7 @@ public interface IOrderBook {
     /**
      * @return the unique identifier of the security this order book is for
      */
-    public String getSecurity();
+    public Security getSecurity();
 
     /**
      * @param reader visitor pattern for reading the order book
--- TradingEngine/src/main/java/org/parthiv/orderbook/IOrderVisitor.java
+++ TradingEngine/src/main/java/org/parthiv/orderbook/IOrderVisitor.java
@@ -3,6 +3,10 @@ package org.parthiv.orderbook;
 import org.parthiv.orderbook.orders.LimitOrder;
 import org.parthiv.orderbook.orders.MarketOrder;
 
+/**
+ * Visitor pattern for Orders
+ */
+
 public interface IOrderVisitor {
     public void visit(LimitOrder order);
     public void visit(MarketOrder order);
